39e683420182e37e82edd563d9cc3d8c23e0cff9
Kevin Rankin <kevin.g.rankin@gmail.com>
2020-09-27 12:49:54 -0400

    adding first tests

--- shorty/lib/shorty_web/controllers/page_controller.ex
+++ shorty/lib/shorty_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule ShortyWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def find(conn, params = %{"slug" => slug} ) do
+    redirect(conn, external: "http://google.com")
+  end
 end
--- shorty/test/shorty_web/controllers/page_controller_test.exs
+++ shorty/test/shorty_web/controllers/page_controller_test.exs
@@ -1,8 +1,21 @@
 defmodule ShortyWeb.PageControllerTest do
   use ShortyWeb.ConnCase
 
+  alias Shorty.Link
+
+  def generate_slug do
+    {:ok, slug} = Link.create_slug("http://foobar.com")
+    slug
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+
+    test "GET /slug", %{conn: conn} do
+    slug = generate_slug()
+    conn = get(conn, "/#{slug}")
+    assert html_response(conn, 302) =~ "redirected"
   end
 end
